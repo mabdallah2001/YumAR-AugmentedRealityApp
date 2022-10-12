bdb5060d27e0102909629f9a71cf853a7f6a1f13
Santiago Zaldivar <santiago.zaldivar.1997@gmail.com>
2022-10-12 14:01:13 +1100

    add proxy to route api request to django server

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,15 @@
+import { QueryClient } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
