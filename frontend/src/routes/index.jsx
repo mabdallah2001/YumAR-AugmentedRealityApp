fa158aef96b297f96febbad7b37240e7aa104311
Santiago Zaldivar <santiago.zaldivar.1997@gmail.com>
2022-10-24 21:21:11 +1100

    reset homepage file

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,8 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
 
 export const CustomerHomePage = () => {
-  const {data, isLoading} = useQuery()
 
   return <div>Customer Home page</div>;
 };
