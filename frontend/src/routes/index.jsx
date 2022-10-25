7fadc4e710a9307398d2a2bb4bc58787c3f61982
Adrian Currington <acur4177@uni.sydney.edu.au>
2022-10-25 22:34:46 +1100

    Basic start to frontend

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,5 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 
 export const CustomerHomePage = () => {
 
-  return <div>Customer Home page</div>;
+  const {data, isLoading, error, isError} = useQuery(['api/v1/menu', async () => {
+    let response = await fetch("/v1/menu", {method: "GET"});
+    if (!response.ok) {
+      throw new Error("Could not retrieve menu items");
+    }
+    return await response.json();
+  }])
+
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error</div>
+
+  return (
+    <div>
+      {data.name};
+    </div>
+  );
 };
