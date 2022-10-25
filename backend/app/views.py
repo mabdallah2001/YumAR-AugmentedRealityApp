df173a02c43ac7cf21e60b225b1b011555bf6582
Adrian Currington <acur4177@uni.sydney.edu.au>
2022-10-25 18:57:16 +1100

    Fixing up endpoint

--- backend/app/views.py
+++ backend/app/views.py
@@ -4,21 +4,14 @@ from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from .models import Restaurant
 
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
 # Create your views here.
 
 from models import MenuItem
 
-@api_view(['GET'])
+@require_http_methods(['GET'])
 def get_menu_items(request):
-    if request.method == 'GET':
-        data = MenuItem.objects.all()
-        return Response(data)
-
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+    data = list(MenuItem.objects.all())
+    return JsonResponse(data, safe=False, status=200)
 
 @require_http_methods(['GET'])
 def example_route(request):
