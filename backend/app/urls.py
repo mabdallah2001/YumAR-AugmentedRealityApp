03dd211d2e1606be814417e3128fb51793343751
Adrian Currington <acur4177@uni.sydney.edu.au>
2022-10-25 18:53:42 +1100

    Backend connection

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, re_path
 
+from . import views
+
 # Add URLS for endpoints located in views.py here
 urlpatterns = [
+    re_path(r'^menu/', views.get_menu_items),
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 # Create your views here.
+
+from models import MenuItem
+
+@api_view(['GET'])
+def get_menu_items(request):
+    if request.method == 'GET':
+        data = MenuItem.objects.all()
+        return Response(data)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
