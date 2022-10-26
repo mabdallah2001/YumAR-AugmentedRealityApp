d46f6df59ddb61adc28be29fa9af0afcfa4c0a02
Adrian Currington <acur4177@uni.sydney.edu.au>
2022-10-26 20:07:21 +1100

    backend finalised

--- backend/app/views.py
+++ backend/app/views.py
@@ -2,6 +2,8 @@ from math import fabs
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
+from django.core import serializers;
+
 from .models import Restaurant
 
 # Create your views here.
@@ -10,7 +12,8 @@ from .models import MenuItem
 
 @require_http_methods(['GET'])
 def get_menu_items(request):
-    data = list(MenuItem.objects.all())
+    data_json = serializers.serialize("json", MenuItem.objects.all())
+    data = {"menu": data_json}
     return JsonResponse(data, safe=False, status=200)
 
 @require_http_methods(['GET'])
