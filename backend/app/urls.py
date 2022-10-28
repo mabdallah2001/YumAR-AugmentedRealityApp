626d30ab62497b36b3959ca1b46b7b0090a70607
Dina <dkaz5711@uni.sydney.edu.au>
2022-10-28 21:49:21 +1100

    items detail page done

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
-from .views import example_route
+from .views import example_route,get_menu_item
 
 # Add URLS for endpoints located in views.py here
 urlpatterns = [
-  path('example/', example_route)
+  path('example/', example_route),
+  path('item/<int:id>/',get_menu_item)
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -2,10 +2,16 @@ from math import fabs
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
-from .models import Restaurant
+from .models import Restaurant,MenuItem
 
 # Create your views here.
 @require_http_methods(['GET'])
 def example_route(request):
     data = list(Restaurant.objects.values())
+    return JsonResponse(data, safe=False, status=200)
+
+@require_http_methods(['GET'])
+def get_menu_item(request,id):
+    data = list(MenuItem.objects.filter(id=id).values())
+    print (data)
     return JsonResponse(data, safe=False, status=200)
