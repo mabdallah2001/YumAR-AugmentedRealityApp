8cf3121e0b49aa5aef5b831297334d53e135100e
Mohammed Abdallah <mabd5880@uni.sydney.edu.au>
2022-10-28 15:19:30 +1100

    backend in progress

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, re_path
 
+from views import example_route, getOrder
+
 # Add URLS for endpoints located in views.py here
+# urlpatterns = [
+#   path('example/', example_route)
+# ]
+
 urlpatterns = [
+  path('order/', getOrder)
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from models import Order
+from django.http import JsonResponse
 
-# Create your views here.
+
+# # Create your views here.
+# @require_http_methods(['GET'])
+# def example_route(request):
+#     data = list(Restaurant.objects.values())
+#     return JsonResponse(data, safe=False, status=200)
+
+@require_http_methods(['GET'])
+def getOrder(request):
+    data = list(Order.objects.values())
+    return JsonResponse(data, safe=False, status=200)
